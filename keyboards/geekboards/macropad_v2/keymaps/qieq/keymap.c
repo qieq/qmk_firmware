fe03b93f3d7c9a54cced68b113e2e0471e66a344
qieq <25284336+qieq@users.noreply.github.com>
2023-11-26 20:06:23 +0300

    wip
    wip

--- keyboards/geekboards/macropad_v2/keymaps/qieq/keymap.c
+++ keyboards/geekboards/macropad_v2/keymaps/qieq/keymap.c
@@ -18,28 +18,46 @@
 
 #define _MEDIA 0
 #define _FL 1
-#define _CONTROLS 2
-#define _RGB 3
+#define _RGB 2
 
 const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_MEDIA] = LAYOUT_ortho_2x4(
+        /*
+        FL layer            F23
+        play/pause          left CTRL(Mute)
+        prev track          vol up
+        next track (RGB)    vol down
+        */        
         LT(_RGB, KC_MNXT), KC_MPRV,       KC_MPLY,          DF(1),
         KC_VOLD,           KC_VOLU,       LCTL_T(KC_MUTE),  KC_F23
+
     ),
     [_FL] = LAYOUT_ortho_2x4(
-        KC_LCTL,           KC_C,          LCTL(LALT(KC_Z)), DF(2),
+        /*
+        MEDIA layer         space
+        undo                set mixer track
+        tool cut            tool mute
+        left CTRL           tool pencil
+        */
+        KC_LCTL,           KC_C,          LCTL(LALT(KC_Z)), DF(0),
         LSFT_T(KC_P),      KC_T,          LCTL(KC_L),       KC_SPC
     ),
-    [_CONTROLS] = LAYOUT_ortho_2x4(
-        KC_MS_L,           KC_MS_D,       KC_BTN1,          DF(0), 
-        KC_MS_R,           KC_MS_U,       KC_BTN2,          LALT(KC_TAB)
-    )
-    ,[_RGB] = LAYOUT_ortho_2x4(
+    [_RGB] = LAYOUT_ortho_2x4(
+        /*
+        hue--               hue++
+        saturation--        saturation++
+        brightness--        brightness++
+        hold to RGB         toggle lighting
+        */
         KC_TRNS,           RGB_VAD,       RGB_SAD,          RGB_HUD, 
         RGB_TOG,           RGB_VAI,       RGB_SAI,          RGB_HUI
     )
 };
 
+void keyboard_post_init_user(void) {
+    rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
+    rgb_matrix_sethsv_noeeprom(HSV_OFF);
+}
 // bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
 //     rgb rgb = {0, 255, 255};
 
@@ -56,6 +74,11 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //     return false;
 // }
 
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    uprintf("\nkeypress: col: %i, row: %i", record->event.key.col, record->event.key.row);
+    return true;
+}
+
 layer_state_t default_layer_state_set_user(layer_state_t state) {
     RGB rgb = { 0, 0, 0 };
 
@@ -66,12 +89,9 @@ layer_state_t default_layer_state_set_user(layer_state_t state) {
         case _FL:
             rgb.r = 0; rgb.g = 255, rgb.b = 0;
             break;
-        case _CONTROLS:
-            rgb.r = 0; rgb.g = 0, rgb.b = 255;
-            break;
-        case _RGB:
-            rgb.r = 130; rgb.g = 0, rgb.b = 255;
-            break;
+        // case _RGB:
+        //     rgb.r = 130; rgb.g = 0, rgb.b = 255;
+        //     break;
         default: //  for any other layers, or the default layer
             rgb.r = 10; rgb.g = 100, rgb.b = 200;
             break;
@@ -79,7 +99,8 @@ layer_state_t default_layer_state_set_user(layer_state_t state) {
 
     uprintf("\nrgb: %i, %i, %i", rgb.r, rgb.g, rgb.b);
 
-    rgb_matrix_step();
-
+    for(int i; i<21; i++){
+    rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
+    }
     return state;
 }
